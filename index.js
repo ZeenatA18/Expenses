7d2e74b076156f550cfa71e2899eebaae42bab09
Zeenat <zeenatavontuur@gmail.com>
2022-09-26 13:56:17 +0200

    zee

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const flash = require('express-flash');
+const session = require('express-session');
+const app = express();
+const pgp = require('pg-promise')();
+
+let useSSL = false;
+let local = process.env.LOCAL || false;
+if (process.env.DATABASE_URL && !local) {
+    useSSL = true;
+}
+
+const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://<user>:<password>@localhost:5432/<databasename>';
+
+const config = {
+    connectionString: DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
+
+const db = pgp(config);
+const expenses_instance = expenses(db);
+
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(session({
+    secret: "This is my long String that is used for session",
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(flash());
